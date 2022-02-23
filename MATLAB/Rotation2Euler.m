4e132201cd50b6f5da6b859c0162191449b24d93
arvidsorfeldt <arvid.sorfeldt@gmail.com>
2022-02-23 09:56:12 +0100

    Update structure

--- MATLAB/Rotation2Euler.m
+++
@@ -1,8 +0,0 @@
-function [Eul] = Rotation2Euler(Rot)
-th_x = atan2d(Rot(3,2),Rot(3,3));
-th_y = atan2d(-Rot(3,1), sqrt(Rot(3,2)^2 + Rot(3,3)^2));
-th_z = atan2d(Rot(2,1),Rot(1,1));
-
-Eul = [th_x, th_y, th_z];
-end
-
