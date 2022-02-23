5727b9825dae99e9fd41f0b5d71435fd01b33b8e
arvidsorfeldt <arvid.sorfeldt@gmail.com>
2022-02-23 13:53:36 +0100

    added ability to run the runner

--- point_estimation/runner.py
+++ point_estimation/runner.py
@@ -5,3 +5,7 @@ def run():
     pcd = o3d.io.read_point_cloud(
         'point_estimation/data/realsense_testcase2.ply')
     o3d.visualization.draw_geometries([pcd])
+
+
+if __name__ == '__main__':
+    run()
