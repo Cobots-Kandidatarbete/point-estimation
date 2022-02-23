e43c8adb9738294641db63a6bda0a7e2b246f753
arvidsorfeldt <arvid.sorfeldt@gmail.com>
2022-02-23 20:46:33 +0100

    Testar clusterigenkänning

--- point_estimation/runner.py
+++ point_estimation/runner.py
@@ -1,6 +1,7 @@
 import open3d as o3d
 import copy
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def run():
@@ -8,8 +9,20 @@ def run():
         'point_estimation/data/realsense_testcase2.ply')
     # o3d.visualization.draw_geometries([pcd])
     frame = o3d.geometry.TriangleMesh.create_coordinate_frame()
-    rotated_pcd = rotate_pc(pcd)
-    o3d.visualization.draw_geometries([rotated_pcd, frame])
+    rotated_pcd, plane_model, inliers = rotate_pc(pcd)
+    rotated_pcd = rotated_pcd.select_by_index(inliers, invert=True)
+
+    with o3d.utility.VerbosityContextManager(o3d.utility.VerbosityLevel.Debug) as cm:
+        labels = np.array(
+            rotated_pcd.cluster_dbscan(eps=0.1, min_points=100, print_progress=True))
+
+    max_label = labels.max()
+    print(f"point cloud has {max_label + 1} clusters")
+    colors = plt.get_cmap("tab20")(
+        labels / (max_label if max_label > 0 else 1))
+    colors[labels < 0] = 0
+    rotated_pcd.colors = o3d.utility.Vector3dVector(colors[:, :3])
+    o3d.visualization.draw_geometries([rotated_pcd, pcd, frame])
 
 
 def rotate_pc(pcd):
@@ -24,7 +37,7 @@ def rotate_pc(pcd):
     v = np.matrix([[0, -cross[2], cross[1]], [cross[2],
                   0, -cross[0]], [-cross[1], cross[0], 0]])
     R = np.eye(3) + v + np.matmul(v, v)*1/(1+c)
-    return copy.deepcopy(pcd).rotate(R)
+    return [copy.deepcopy(pcd).rotate(R), plane_model, inliers]
 
 
 if __name__ == '__main__':
