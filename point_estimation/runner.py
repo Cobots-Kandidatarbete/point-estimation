e5a868e74c68587c1e4f076a31bebef592a361ad
arvidsorfeldt <arvid.sorfeldt@gmail.com>
2022-02-23 10:13:34 +0100

    Added data files and read/vizualize pc

--- point_estimation/runner.py
+++ point_estimation/runner.py
@@ -1,2 +1,8 @@
+import numpy as np
+import open3d as o3d
+
+
 def run():
-    print("Hello World!")
+    pcd = o3d.io.read_point_cloud(
+        'point_estimation/data/realsense_testcase2.ply')
+    o3d.visualization.draw_geometries([pcd])
